ed8a5298318cc307d0d8e1a8aaab193846ce3e65
laurenfb <laurenfriesbrink@gmail.com>
2016-09-28 15:59:23 -0700

    added show method in controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,18 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
+    @tasks = Task.all
+    @current_task = nil
+
+    @tasks.each do |task|
+      number = params[:id].to_i
+      if task[:id] == number
+        @current_task = task
+      end
+    end
   end
 
   def new
