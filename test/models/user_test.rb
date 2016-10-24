8cbf9b8179685b8b7ba4279bcad2d8aff20d789e
laurenfb <laurenfriesbrink@gmail.com>
2016-10-24 13:29:25 -0700

    shell for user model testing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  let(:ada) { users(:ada) }
+
+  test "the truth" do
+    assert true
+  end
+
+  test 'users can have tasks' do
+
+  end
+
+  test 'users have the correct tasks' do
+
+  end
+
+  test 'users do not have others\' tasks' do
+
+  end
+
+  test 'new users have no tasks yet' do
+
+  end
+
+  test 'users must have uid' do
+
+  end
+
+  test 'users must have provider' do
+
+  end
+
+  test 'users must have email' do
+
+  end
+
+  test 'build_from_github creates a user' do
+
+  end
 end
